cdd9ee5a807967522fec27d0b4dcfd30e24949fc
Rowan Allan <rowanallan11@Rowans-MacBook-Air.local>
2026-05-08 10:45:51 +0100

    log file creation

--- hpc/fragment.sh
+++ hpc/fragment.sh
@@ -2,10 +2,12 @@
 #PBS -l walltime=4:00:00
 #PBS -l select=1:ncpus=1:mem=50gb
 #PBS -N aav_fragmentation
-#PBS -o logs/01_fragment.o
-#PBS -e logs/01_fragment.e
+#PBS -o hpc/logs/01_fragment.o
+#PBS -e hpc/logs/01_fragment.e
 
 cd /general/user/rma25/projects/hda_25-26/live/TDS/rowzang/final_project/aav-tcr-epitope-pipeline
+
+mkdir -p hpc/logs data/output/fragmentation
 
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate AAV
--- hpc/mhcflurry.sh
+++ hpc/mhcflurry.sh
@@ -2,14 +2,16 @@
 #PBS -l walltime=4:00:00
 #PBS -l select=1:ncpus=1:mem=50gb
 #PBS -N aav_mhcflurry
-#PBS -o logs/01_mhcflurry.o
-#PBS -e logs/01_mhcflurry.e
+#PBS -o hpc/logs/01_mhcflurry.o
+#PBS -e hpc/logs/01_mhcflurry.e
 
 cd /general/user/rma25/projects/hda_25-26/live/TDS/rowzang/final_project/aav-tcr-epitope-pipeline
+
+mkdir -p hpc/logs data/output/fragmentation data/output/mhcflurry
 
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate AAV
 
-export MHCFLURRY_DATA_DIR="/general/user/rma25/projects/hda_25-26/live/TDS/rowzang/final_project/mhcflurry_data"
+export MHCFLURRY_DATA_DIR="/general/user/rma25/projects/hda_25-26/live/TDS/rowzang/final_project/mhcflurry"
 
 python scripts/run_mhcflurry.py
