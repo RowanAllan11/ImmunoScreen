3976a73666768e685b3489237700ed52db58f3a8
Rowan Allan <rowanallan11@Rowans-MacBook-Air.local>
2026-05-08 11:40:52 +0100

    MHC file path

--- hpc/mhcflurry.sh
+++ hpc/mhcflurry.sh
@@ -12,11 +12,12 @@ mkdir -p hpc/logs data/output/fragmentation data/output/mhcflurry
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate AAV
 
-export MHCFLURRY_DATA_DIR="/general/project/hda_25-26/live/TDS/rowzang/final_project/mhcflurry"
+export MHCFLURRY_DATA_DIR="/rds/general/project/hda_25-26/live/TDS/rowzang/final_project/mhcflurry"
 mkdir -p "$MHCFLURRY_DATA_DIR"
 
+echo "PWD=$(pwd)"
 echo "MHCFLURRY_DATA_DIR=$MHCFLURRY_DATA_DIR"
 mhcflurry-downloads path || true
-which mhcflurry-predict
+
 
 python scripts/run_mhcflurry.py
