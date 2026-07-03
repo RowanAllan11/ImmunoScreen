7a3e058f36e9527e87b13278ca708971a13f15aa
Rowan Allan <rowanallan11@Rowans-MacBook-Air.local>
2026-07-03 16:19:35 +0100

    combine function fix

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -171,6 +171,12 @@ def run_combination(
             / run_label
             / "predictions_mapped.tsv"
         ),
+        "--variant-id-col",
+        settings["variant_id_column"],
+        "--library-id-col",
+        settings["library_id_column"],
+        "--seq-col",
+        settings["sequence_column"],
         "--i",
         settings["library"],
         "--var-start",
